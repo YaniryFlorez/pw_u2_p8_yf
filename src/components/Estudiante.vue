2a55995adc374278969f9db5ed7a7d5c023e8c93
Yaniry Florez <ymflorez@gmail.com>
2025-05-15 21:23:20 -0500

    Tarea 3 Formulario Estudiante Vue

--- src/components/Estudiante.vue
+++ src/components/Estudiante.vue
@@ -1,49 +1,58 @@
 <template>
   <div class="container">
     <label for="id_nombre">Nombre</label>
-    <input v-model="nuevoNombre" id="id_nombre" type="text">
+    <input v-model="nuevoNombre" id="id_nombre" type="text" />
     <label for="id_apellido">Apellido</label>
-     <input v-model="nuevoApellido" id="id_apellido" type="text">
+    <input v-model="nuevoApellido" id="id_apellido" type="text" />
+    <label for="id_fecha_nacimiento">Fecha Nacimiento</label>
+    <input v-model="nuevaFecha" id="id_fecha_nacimiento" type="date" />
+    <label for="id_genero">Genero</label>
+    <input v-model="nuevoGenero" id="id_genero" type="text" />
+    <label for="id_cedula">Cedula</label>
+    <input v-model="nuevaCedula" id="id_cedula" type="text" />
     <button v-on:click="agragarEstudiante()">Agregar Estudiante</button>
-    
-   
+
     {{ nuevoNombre }}
 
     <ul>
-      <li v-for="{nombre, apellido} in lista" :key="nombre">
-        Nombre:{{ nombre }}-Apellido:{{ apellido }}</li>
-     
-
+      <li v-for="{ nombre, apellido, fecha, genero, cedula } in lista" :key="nombre">
+        Nombre:{{ nombre }}-Apellido:{{ apellido }}-
+        Fecha Nacimiento:{{ fecha }}-Genero:{{ genero }}-
+        Cedula:{{ cedula }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            nuevoNombre: "nuevo",
-            lista:[
-                {nombre:'Yaniry', apellido:'Florez'},
-                {nombre:'Daniel', apellido:'Lopez'},
-                {nombre:'Pedro', apellido:'Ruiz'},
-                {nombre:'Andrea', apellido:'Rivera'},
-                {nombre:'Carlos', apellido:'Vargas'},
-                {nombre:'Ana', apellido:'Enriquez'},
-                {nombre:'Viviana', apellido:'Lopez'},
-            ]
-        }
-    },
-    methods:{
-        agragarEstudiante(){
-            const nuevo={
-                nombre:this.nuevoNombre,
-                apellido: this.nuevoApellido,
-            };
-            this.lista.unshift(nuevo);
+  data() {
+    return {
+      nuevoNombre: "nuevo",
+      lista: [
+        { nombre: "Yaniry", apellido: "Florez" },
+        { nombre: "Daniel", apellido: "Lopez" },
+        { nombre: "Pedro", apellido: "Ruiz" },
+        { nombre: "Andrea", apellido: "Rivera" },
+        { nombre: "Carlos", apellido: "Vargas" },
+        { nombre: "Ana", apellido: "Enriquez" },
+        { nombre: "Viviana", apellido: "Lopez" },
+      ],
+    };
+  },
+  methods: {
+    agragarEstudiante() {
+      const nuevo = {
+        nombre: this.nuevoNombre,
+        apellido: this.nuevoApellido,
+        fecha: this.nuevaFecha,
+        genero: this.nuevoGenero,
+        cedula: this.cedula,
 
-        },
+      };
+      this.lista.unshift(nuevo);
     },
+  },
 };
 </script>
 
